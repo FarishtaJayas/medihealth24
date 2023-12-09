8ad4e928ccc590e9193d0ef1fffcc9445d7e5fb8
FarishtaJayas <farishtajayas@gmail.com>
2023-12-09 14:36:08 +0600

    final changes

--- medihealth24/settings.py
+++ medihealth24/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-$@bd*hqwld@wpbv04@evd7@_%wf*82k7=t+km1+-=4ozo)ze0z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
