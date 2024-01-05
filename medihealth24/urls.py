1d63560c527b4503881f2df3d9644a2b559c0e4a
farishtajayas <farishtajayas@gmail.com>
2024-01-06 03:42:35 +0600

    * Made manufacturers searchable

--- medihealth24/urls.py
+++ medihealth24/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('medicines.urls')),
+    path("select2/", include("django_select2.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
